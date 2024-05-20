d3effb70d609b9d65e9661564ce649899c8660b9
wca091 <165815049+wca091@users.noreply.github.com>
2024-05-20 17:54:29 -0300

    Add files via upload

---
+++ ultima atualização projeto faci/main.js
@@ -0,0 +1,44 @@
+class MobileNavbar {
+    constructor(Menu, navList, navLinks) {
+      this.Menu = document.querySelector(Menu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.Menu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.Menu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.Menu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
